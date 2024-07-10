d8a6126b0a71b84c57505454518f6580217ba302
Danny Murphy <dmurphy@forbesadvisor.com>
2024-07-10 08:04:25 +0100

    Removed logging

--- src/introl.timesheets.api/Controllers/TimesheetController.cs
+++ src/introl.timesheets.api/Controllers/TimesheetController.cs
@@ -9,31 +9,20 @@ namespace Introl.Timesheets.Api.Controllers;
 [ApiController]
 public class TimesheetController(
     IWorksheetReader worksheetReader, 
-    IWorksheetWriter worksheetWriter,
-    ILogger<TimesheetController> logger) : Controller
+    IWorksheetWriter worksheetWriter) : Controller
 {
     [HttpPost("process")]
     public IActionResult ProcessTimeSheet(IFormFile model)
     {
-        Console.WriteLine("Processing timesheet...");
-        logger.LogWarning("Processing timesheet...");
         using var x = model.OpenReadStream();
         var workbook = new XLWorkbook(model.OpenReadStream());
         
-        Console.WriteLine("Starting reader");
-        logger.LogWarning("Starting reader");
         var inputSheetModel = worksheetReader.Process(workbook);
-        Console.WriteLine("After reader");
-        logger.LogWarning("After reader");
         using var output = new XLWorkbook();
         worksheetWriter.Process(inputSheetModel, output);
-        Console.WriteLine("After writer");
-        logger.LogWarning("After writer");
         
         using var stream = new MemoryStream();
         output.SaveAs(stream);
-        Console.WriteLine("After save");
-        logger.LogWarning("After save");
         return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", $"{model.FileName}_processed.xlsx");
     }
 }
--- src/introl.timesheets.api/Program.cs
+++ src/introl.timesheets.api/Program.cs
@@ -3,8 +3,6 @@ using Introl.Timesheets.Api.services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
