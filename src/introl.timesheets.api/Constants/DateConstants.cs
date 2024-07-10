9571d524634f2134025935327bf7c8e33b624b98
Danny Murphy <dmurphy@forbesadvisor.com>
2024-07-10 17:11:53 +0100

    deleting project week cell

--- src/introl.timesheets.api/Constants/DateConstants.cs
+++
@@ -1,6 +0,0 @@
-namespace Introl.Timesheets.Api.Constants;
-
-public static class DateConstants
-{
-    public static DateOnly ProjectStartDate => new DateOnly(2024, 5, 20);
-}
--- src/introl.timesheets.api/Models/InputSheetModel.cs
+++ src/introl.timesheets.api/Models/InputSheetModel.cs
@@ -9,13 +9,4 @@ public class InputSheetModel
     public required DateOnly EndDate { get; init; }
     public required IEnumerable<Employee> Employees { get; init; }
     public required IXLWorksheet RawTimesheetsWorksheet { get; init; }
-
-    public int WeekNumber
-    {
-        get
-        {
-            var days = StartDate.DayNumber - DateConstants.ProjectStartDate.DayNumber;
-            return (days / 7) + 1;
-        }
-    }
 }
