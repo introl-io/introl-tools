b2f5a5fd47852919a6658d1b03b3e582eb53ff59
Danny Murphy <dmurphy@forbesadvisor.com>
2024-07-11 08:18:47 +0100

    changing employees to list

--- src/introl.timesheets.api/Services/WorksheetReader.cs
+++ src/introl.timesheets.api/Services/WorksheetReader.cs
@@ -33,16 +33,19 @@ public class WorksheetReader(IWorksheetReaderHelper worksheetReaderHelper) : IWo
         return cell.Address.ColumnNumber;
     }
 
-    private IEnumerable<Employee> GetEmployees(IXLWorksheet worksheet)
+    private IList<Employee> GetEmployees(IXLWorksheet worksheet)
     {
         var employeeRow = GetFirstEmployeeRow(worksheet);
         var dayColDict = worksheetReaderHelper.GetDayOfTheWeekColumnDictionary(worksheet);
         var ratesCol = RatesColumn(worksheet);
+        var employees = new List<Employee>();
         do
         {
-            yield return GetEmployee(worksheet, employeeRow, dayColDict, ratesCol, out var numRowsUsedByEmployee);
+            employees.Add(GetEmployee(worksheet, employeeRow, dayColDict, ratesCol, out var numRowsUsedByEmployee));
             employeeRow += numRowsUsedByEmployee;
         } while (!string.IsNullOrEmpty(worksheet.Cell(employeeRow, 1).GetString()));
+
+        return employees;
     }
     
     private Employee GetEmployee(IXLWorksheet worksheet, int employeeRow, IDictionary<DayOfTheWeek, int> dayDictionary, int ratesCol, out int numRowsUsedByEmployee)
