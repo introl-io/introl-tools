72e29f477adc7265ef9cc5f687ba0bd733237201
Danny Murphy <dmurphy@forbesadvisor.com>
2024-07-11 12:07:03 +0100

    renamed env var

--- src/introl.timesheets.api/Constants/AuthorizationConstants.cs
+++ src/introl.timesheets.api/Constants/AuthorizationConstants.cs
@@ -3,6 +3,6 @@ namespace Introl.Timesheets.Api.Constants;
 public static class AuthorizationConstants
 {
     public static string ApiKeyHeader => "X-API-Key";
-    public static string ApiKeyEnvVariable => "API-KEY";
+    public static string ApiKeyEnvVariable => "API_KEY";
     
 }
