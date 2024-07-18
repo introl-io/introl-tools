fd4d581cb1d1e186cac9366498ad785a2a708ead
Danny Murphy <dmurphy@forbesadvisor.com>
2024-07-18 11:01:56 +0100

    more constants

--- src/introl.timesheets.api/Constants/InputWorkbookConstants.cs
+++ src/introl.timesheets.api/Constants/InputWorkbookConstants.cs
@@ -2,6 +2,9 @@ namespace Introl.Timesheets.Api.Constants;
 
 public static  class InputWorkbookConstants
 {
-    public static string WeekCellValue => "week";
-    public static string TypeCellValue => "type";
+    public static string WeekCellValue => "Week";
+    public static string TypeCellValue => "Type";
+
+    public static string RegularHours => "Regular Hours";
+    public static string WeeklyOt => "Weekly OT";
 }
