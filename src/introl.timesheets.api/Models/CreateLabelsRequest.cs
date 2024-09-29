9c9db15ce4487bf3c58c54dac52bc8c4b42aaf39
Danny Murphy <dmurphy@forbesadvisor.com>
2024-09-29 16:50:14 +0100

    updating description

--- src/introl.timesheets.api/Models/CreateLabelsRequest.cs
+++ src/introl.timesheets.api/Models/CreateLabelsRequest.cs
@@ -4,7 +4,7 @@ namespace Introl.Timesheets.Api.Models;
 
 public class CreateLabelsRequest
 {
-    [SwaggerSchema(Description = "File must be a .xlsx file with the port mappings in the first sheet")]
+    [SwaggerSchema(Description = "File must be a .csv, or .xlsx file with the port mappings in the first sheet")]
     public required IFormFile File { get; init; }
     [SwaggerSchema(Description = "Format to use for source port in label. Put column letter or number in {} to be replaced with the column value. eg. {A}.R{2}.{E}")]
     public required string SourcePortLabelFormat { get; init; }
