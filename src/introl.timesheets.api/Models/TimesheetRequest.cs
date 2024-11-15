bd02fa51419abcbc2ef0a45a6c5ac75eca804c8e
Danny Murphy <danjmurphy92@gmail.com>
2024-11-15 18:49:32 +0000

    Revert "Revert "Renaming from timesheets to tools (#57)" (#58)"
    This reverts commit 6e90572c9758e880e54d1cc1a69f2311fff6cd3e.

--- src/introl.timesheets.api/Models/TimesheetRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using Swashbuckle.AspNetCore.Annotations;
-
-namespace Introl.Timesheets.Api.Models;
-
-public record TimesheetRequest
-{
-    public required IFormFile File { get; init; }
-    public required bool CalculateOvertime { get; init; }
-}
---
+++ tests/introl.tools.api.tests.acceptance/AcceptanceWebHost.cs
@@ -0,0 +1,13 @@
+﻿using Introl.Tools.Api.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Introl.Tools.Api.Tests.Acceptance;
+
+internal class AcceptanceTestsWebHost : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        Environment.SetEnvironmentVariable(AuthorizationConstants.ApiKeyEnvVariable, AcceptanceTestConstants.ApiKey);
+    }
+}
